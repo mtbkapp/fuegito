06790c29f568ac5d891bbfdd8ee89be742f2204e
Jason Kapp <mtbkapp@gmail.com>
2013-09-20 23:25:50 -0500

    Almost got things going into mongo.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [enlive "1.1.4"]]
+                 [enlive "1.1.4"]
+                 [com.novemberain/monger "1.5.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler fuegito.handler/app}
   :profiles
--- src/fuegito/handler.clj
+++ src/fuegito/handler.clj
@@ -1,14 +1,23 @@
 (ns fuegito.handler
   (:use compojure.core)
   (:require [fuegito.views :as v]
+            [fuegito.storage :as s]
+            [ring.util.response :as resp]
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn to-keyword-keys
+  [m]
+  (apply hash-map (flatten (map (fn [[k v]] [(keyword k) v]) m))))
 
 (defroutes app-routes
-  (GET "/" [] (v/home)) 
+  (GET "/" [] (do (s/setup-db!) (v/home)))
   (GET "/create" [] (v/create [] {})) 
-  (POST "/create" [] "recieves the burn down form and redirects")
+  (POST "/create" {:keys [form-params]}
+        (let [messages (s/valid-chart? form-params)]
+          (if (empty? messages)
+            (resp/redirect-after-post (str (s/create-chart form-params)))
+            (v/create messages (to-keyword-keys form-params)))))
   (GET "/:id" [id] "returns the main burn down page for the id")
   (GET "/:id/chart" [id] "returns the image of the chart")
   (POST "/:id/add-data" [] "adds data then redirects back to page")
@@ -17,3 +26,5 @@
 
 (def app
   (handler/site app-routes))
+
+
